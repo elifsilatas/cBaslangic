09e6383bd82215ecd422269c064a3480276f145d
Elif Sıla Taş <122424595+elifsilatas@users.noreply.github.com>
2023-11-29 18:07:46 +0300

    cBaslangic
    c++ çalışırken başlangıç düzey kodlarım

---
+++ Untitled1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;//tipi, ismi
+	a = 10;//değer
+	cout << a <<endl;
+	a = 20;
+	cout << "Bandirma  " << a << endl;
+	int b = 30;
+	cout << b << endl;
+	cout << "Benim yasim: " << 25 << endl;
+	cout << "guncel a degeri: " << 68 << endl;
+	float pi = 3.14;
+	cout << pi << endl;
+	long tl = 88768768;
+	cout << tl << endl;
+	char t = 'G';
+	cout << t << endl;
+	bool deger = true;
+	cout << deger << endl;
+	int tipDonusumu = t;
+	cout << tipDonusumu << endl;
+	int ipi = pi;
+	cout << ipi << endl;
+	int ideger = deger;
+	cout << ideger << endl;
+	int itl = tl;
+	cout << itl << endl;
+	int v = 38;
+	cout << v << endl;
+	char av = v;
+	cout << av << endl;
+	cout << (char)v << endl;//type casting
+	cout << (char)36 << endl;
+	return 0;
+	
+
+}
+//yorum satırı yapmak için kullanılan sembol.
+/*
+birden fazla
+satırda
+yorum yapmak
+için kullanılan
+semboller.
+*/
